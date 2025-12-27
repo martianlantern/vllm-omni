460336af606a0e8670ca4dcf48a18ce2c8eb9796
martianlantern <martianlantern@gmail.com>
2025-12-27 18:49:52 +0530

    adding export for SwiGLUFFNF
    Signed-off-by: martianlantern <martianlantern@gmail.com>

--- vllm_omni/model_executor/models/vggt_model/vggt_source/layers/__init__.py
+++ vllm_omni/model_executor/models/vggt_model/vggt_source/layers/__init__.py
@@ -8,6 +8,6 @@ from .attention import MemEffAttention
 from .block import NestedTensorBlock
 from .mlp import Mlp
 from .patch_embed import PatchEmbed
-from .swiglu_ffn import SwiGLUFFN
+from .swiglu_ffn import SwiGLUFFN, SwiGLUFFNFused
 
-__all__ = ["MemEffAttention", "NestedTensorBlock", "Mlp", "PatchEmbed", "SwiGLUFFN"]
+__all__ = ["MemEffAttention", "NestedTensorBlock", "Mlp", "PatchEmbed", "SwiGLUFFN", "SwiGLUFFNFused"]
